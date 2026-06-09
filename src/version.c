d6c3efe2bec45e0352ba4506febd1686e17cd5a9
Javier Moragon <jamofer@gmail.com>
2026-06-09 02:41:40 +0200

    build: bump to 0.1.2 and derive CAN_HUB_VERSION from the project version

--- src/version.c
+++ src/version.c
@@ -1,5 +1,9 @@
 #include "version.h"
 
+#ifndef CAN_HUB_VERSION
+#define CAN_HUB_VERSION "0.0.0-dev"
+#endif
+
 /* ---------- public ---------- */
 
 const char *Version_String(void)
--- src/version.h
+++ src/version.h
@@ -1,5 +1,3 @@
 #pragma once
 
-#define CAN_HUB_VERSION "0.1.0"
-
 const char *Version_String(void);
